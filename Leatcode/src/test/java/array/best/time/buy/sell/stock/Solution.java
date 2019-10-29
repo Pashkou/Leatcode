9c764cd796c6e31110fc1201fa2611ade8c4c95a
pashkou <pashkou@pashkou-XPS-15-9560>
2019-10-29 15:49:23 +0100

    update

--- Leatcode/src/test/java/array/best/time/buy/sell/stock/Solution.java
+++ Leatcode/src/test/java/array/best/time/buy/sell/stock/Solution.java
@@ -24,54 +24,7 @@ public class Solution {
         return result;
     }
 	
-	public int[] intersect(int[] nums1, int[] nums2) {
-        Map<Integer,Integer> nums1Map = new HashMap<Integer,Integer>();
-        Map<Integer,Integer> nums2Map = new HashMap<Integer,Integer>();
-        
-        for(int i : nums1) {
-        	Integer nums1Counter = nums1Map.get(i);
-        	
-        	if(nums1Counter == null) {
-        		nums1Map.put(i, new Integer(0));
-        	}else {
-        		nums1Map.put(i, ++nums1Counter);
-        	}
-        }
-        
-        for(int i : nums2) {
-        	Integer nums2Counter = nums2Map.get(i);
-        	
-        	if(nums2Counter == null) {
-        		nums2Map.put(i, new Integer(0));
-        	}else {
-        		nums2Map.put(i, ++nums2Counter);
-        	}
-        	
-        }
-        
-        List<Integer> resultList = new ArrayList();
-        for(int i : nums2) {
-        	if(nums1Map.containsKey(i) && nums2Map.containsKey(i)){
-        		Integer integer = nums1Map.get(i);
-        		--integer;
-        		if(integer == 0) {
-        			nums1Map.remove(i);
-        		}else {
-        			nums1Map.put(i, integer);
-        		}
-        		integer = nums2Map.get(i);
-        		--integer;
-        		if(integer == 0) {
-        			nums2Map.remove(i);
-        		}else {
-        			nums2Map.put(i, integer);
-        		}
-        		resultList.add(i);
-        	}
-        }
-        
-        return resultList.stream().mapToInt(p -> p).toArray();
-    }
+	
 	
 	public boolean containsDuplicate(int[] nums) {
 		
