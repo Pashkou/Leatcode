a19d86156bf4d53caa8c0addf6617a6f6a12b0d4
pashkou <pashkou@pashkou-XPS-15-9560>
2019-11-21 16:48:10 +0100

    update

---
+++ Leatcode/src/test/java/kata/BinarySearchTree.java
@@ -0,0 +1,111 @@
+package kata;
+
+import static org.hamcrest.CoreMatchers.containsString;
+
+import org.junit.Test;
+
+public class BinarySearchTree {
+	class Node{
+		Node left, right;
+		Integer val;
+		public Node(Integer value) {
+			this.val = value;
+		}
+	}
+
+	Node root;
+	
+	public Node lookup(Integer value) {
+		if(root == null) return null;
+		
+		if(root.val == value) return root;
+		
+		Node currentNode = root;
+		while(currentNode != null) {
+			if(currentNode.val.equals(value)) {return currentNode;}
+			
+			if(value < currentNode.val) {
+				currentNode = currentNode.left;
+			}
+			if(value > currentNode.val) {
+				currentNode = currentNode.right;
+			}
+			
+		}
+		return null;
+	}
+	
+	public void insert(Integer newValue) {
+		if(root == null) {
+			root = new Node(newValue);
+			return;
+		}
+		Node currentNode = root;
+		while(true) {
+			if(newValue <  currentNode.val) {
+				if(currentNode.left == null) {currentNode.left = new Node(newValue); return;}
+				else {currentNode = currentNode.left;}
+			}
+			
+			if(newValue >  currentNode.val) {
+				if(currentNode.right == null) {currentNode.right = new Node(newValue); return;}
+				else {currentNode = currentNode.right;}
+			}
+		}
+	}
+	
+	public void delete(Integer oldValue) {
+		
+		if(root.val.equals(oldValue)) {
+			root = null; return;
+		}
+		
+		Node currentNode = root;
+		Node parentNode = null;
+		while(true) {
+			if(oldValue < currentNode.val) { 
+				parentNode = currentNode; currentNode = currentNode.left; }
+			if(oldValue > currentNode.val) { 
+				parentNode = currentNode; currentNode = currentNode.right;}
+			if(oldValue == currentNode.val) {
+				if((currentNode.left != null) && (currentNode.right == null)) {
+
+					
+				} else if((currentNode.right != null) && (currentNode.left == null)) {
+					
+				} else {
+					
+				}
+				
+				
+				
+			}
+		}
+		
+		
+	}
+	
+
+	
+	
+	
+	
+	@Test
+	public void test() {
+		BinarySearchTree bst = new BinarySearchTree();
+		
+		bst.insert(9);
+		bst.insert(4);
+		bst.insert(20);
+		bst.insert(1);
+		bst.insert(6);
+		bst.insert(15);
+		bst.insert(170);
+		
+		
+		System.out.println(bst.lookup(170).val);
+
+		
+	}
+
+}
